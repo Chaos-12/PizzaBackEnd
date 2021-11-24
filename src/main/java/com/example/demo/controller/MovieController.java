6944e1247ca2aca5691f37e0954cbf988e48fe2a
David Iglesias <dvd.iglesias@outlook.com>
2021-11-24 13:51:06 +0100

    Query failed to parse

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -3,15 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.domain.Movie;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -21,9 +22,9 @@ public class MovieController {
     private GraphQL graphQL;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<Movie> getAll(@RequestParam String query) {
+    public ResponseEntity<Object> getAll(@RequestBody String query) {
         ExecutionResult execute = graphQL.execute(query);
-        return execute.getData();
+        return new ResponseEntity<>(execute.getData(), HttpStatus.OK);
     }
 
 }
