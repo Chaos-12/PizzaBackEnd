3f9c549d09cebb399dbaf7202b316d3811f051f4
David Iglesias <dvd.iglesias@outlook.com>
2022-01-09 23:33:54 +0100

    fixed problem with Token

--- src/main/java/com/example/demo/security/authFilter/SecurityContextRepository.java
+++ src/main/java/com/example/demo/security/authFilter/SecurityContextRepository.java
@@ -6,6 +6,7 @@ import com.example.demo.core.exceptions.UnauthorizedException;
 import com.example.demo.domain.userDomain.Role;
 import com.example.demo.infraestructure.redisInfraestructure.RedisRepository;
 import com.example.demo.security.UserLogInfo;
+import com.example.demo.security.authTokens.JwtReader;
 import com.example.demo.security.authTokens.TokenProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,7 @@ public class SecurityContextRepository implements ServerSecurityContextRepositor
     private static final String OAUTH_DEFAULT_COOKIE_NAME = "JSESSIONID";
     private final RedisRepository<UserLogInfo, String> infoRepository;
     private final TokenProvider tokenProvider;
+    private final JwtReader jwtReader;
 
     @Override
     public Mono<Void> save(ServerWebExchange serverWebExchange, SecurityContext securityContext) {
@@ -51,23 +53,23 @@ public class SecurityContextRepository implements ServerSecurityContextRepositor
                         .switchIfEmpty(Mono.error(new UnauthorizedException("Wrong format in authorization token")))
                         .flatMap(authHeader -> {
                             String authToken = authHeader.substring(TOKEN_HEADER.length());
-                            return this.authenticateFromRedis(authToken);
+                            String userId = jwtReader.getSubjectFromToken(authToken);
+                            return this.authenticateFromRedis(userId);
                         });
         }
-        try{
+        if (serverWebExchange.getRequest().getCookies().containsKey(OAUTH_DEFAULT_COOKIE_NAME)){
             String sessionId = serverWebExchange.getRequest().getCookies()
                                             .get(OAUTH_DEFAULT_COOKIE_NAME)
                                             .get(0).getValue();
             return this.authenticateFromRedis(sessionId);
-        }catch(Exception ex){
-            return Mono.error(new UnauthorizedException("Authorization is required"));
         }
+        return Mono.error(new UnauthorizedException("Authorization is required"));
     }
 
     private Mono<SecurityContext> authenticateFromRedis(String id){
         return this.infoRepository
                     .getFromID(id)
-                    .switchIfEmpty(Mono.error(new UnauthorizedException("User is logged out")))
+                    .switchIfEmpty(Mono.error(new UnauthorizedException("User is not in Redis DB")))
                     .map(logInfo -> tokenProvider.generateAuthenticationToken(logInfo))
                     .map(SecurityContextImpl::new);
     }
